75203be22d9a29f8de3a145ce6ba6e485920266e
crown0205 <crown0205@gmail.com>
2024-06-14 18:06:21 +0900

    feat : Toc 컴포넌트 추가
    * readme 문서 수정

--- src/app/blog/[...slug]/page.tsx
+++ src/app/blog/[...slug]/page.tsx
@@ -38,42 +38,83 @@ const PostPage = async ({ params }: IPostPageProps) => {
   return (
     <article
       className={cn(
-        // NOTE : prose is from tailwind typography plugin
-        "container prose py-6 max-w-4xl mx-auto",
-        "dark:prose-invert"
+        // NOTE : "prose" is from tailwind typography plugin
+        "w-full px-8 prose py-14 mx-auto max-w-4xl",
+        "flex justify-around",
+        "dark:prose-invert",
+        // NOTE : xl 이상에서만 적용되는 클래스
+        "xl:max-w-6xl xl:px-0"
       )}
     >
-      <div className="flex flex-col mt-10">
-        <h1 className="mb-2 text-4xl font-black">{title}</h1>
-        {description ? (
-          <p className="text-xl m-0 text-muted-foreground">{description}</p>
-        ) : null}
+      <div className={cn("w-full xl:max-w-[800px]")}>
+        <div className="flex flex-col mt-10">
+          <h1 className="mb-2 text-4xl font-black">{title}</h1>
+          {description ? (
+            <p className="text-xl m-0 text-muted-foreground">{description}</p>
+          ) : null}
+          <p className="text-muted-foreground text-sm">
+            {/* [ ] : TAG 컴포넌트 만들기 */}
+            {formatDate(date)} · {tags.join(", ")}
+          </p>
+        </div>
 
-        <p className="text-muted-foreground text-sm">
-          {/* [ ] : TAG 컴포넌트 만들기 */}
-          {formatDate(date)} · {tags.join(", ")}
-        </p>
+        <hr className="my-4" />
+
+        <Image
+          className={cn(
+            "bg-gray-400",
+            "w-[100%] ",
+            "min-h-[200px] max-h-[300px]",
+            "rounded-md border-[2px]"
+          )}
+          src={thumbnail}
+          alt={`thumbnail for ${title}`}
+          width={800}
+          height={400}
+          priority
+          style={{
+            objectFit: "cover",
+          }}
+        />
+
+        <MdxComponent code={body} />
       </div>
-      <hr className="my-4" />
-      <Image
-        className={cn(
-          "bg-gray-400",
-          "w-[100%] ",
-          "min-h-[200px] max-h-[300px]",
-          "rounded-md border-[2px]"
-        )}
-        src={thumbnail}
-        alt={`thumbnail for ${title}`}
-        width={800}
-        height={400}
-        priority
-        style={{
-          objectFit: "cover",
-        }}
-      />
-      <MdxComponent code={body} />
+      <PostToc />
     </article>
   );
 };
 
 export default PostPage;
+
+const PostToc = () => {
+  return (
+    <div
+      className={cn(
+        "hidden xl:block",
+        "sticky top-[160px]",
+        "h-[fit-content] w-[fit-content] min-w-[100px] max-w[150px]",
+        "p-4"
+      )}
+    >
+      <h2 className="text-xl font-bold">Table of Contents</h2>
+
+      <ul className={cn("list-none no-underline", "flex flex-col gap-0")}>
+        <li className="no-underline m-0 p-0">
+          <a className="no-underline hover:underline" href="#test">
+            <small>Test</small>
+          </a>
+        </li>
+        <li className="no-underline m-0 p-0">
+          <a className="no-underline hover:underline" href="#test">
+            <small>Test</small>
+          </a>
+        </li>
+        <li className="no-underline m-0 p-0">
+          <a className="no-underline hover:underline" href="#test">
+            <small>Test</small>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
