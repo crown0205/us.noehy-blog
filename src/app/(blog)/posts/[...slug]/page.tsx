d3e689c340495646fa1610e747d8329109f52952
crown0205 <crown0205@gmail.com>
2024-06-27 16:00:18 +0900

    docs : 블로그 개발기1 추가

--- src/app/(blog)/posts/[...slug]/page.tsx
+++ src/app/(blog)/posts/[...slug]/page.tsx
@@ -1,11 +1,11 @@
 import { posts } from "#site/contents";
+import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import "@/styles/mdx.css";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import PostContent from "./components/PostContent";
 import PostToc from "./components/PostToc";
-import { Metadata } from "next";
-import { siteConfig } from "@/config/site";
 
 interface IPostPageProps {
   params: {
@@ -91,10 +91,11 @@ const PostPage = async ({ params }: IPostPageProps) => {
     <article
       className={cn(
         // NOTE : "prose" is from tailwind typography plugin
-        "w-full px-8 prose py-14 mx-auto max-w-4xl",
+        "w-full px-8 pb-14 pt-0 mx-auto max-w-4xl",
         "flex justify-around",
-        "dark:prose-invert",
-        // NOTE : xl 이상에서만 적용되는 클래스
+        "prose dark:prose-invert",
+        // NOTE :반응형 디자인
+        "md:pt-5 lg:pt-10",
         "xl:max-w-6xl xl:px-0"
       )}
     >
