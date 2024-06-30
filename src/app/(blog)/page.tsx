97bf6a0946401ddb4e47c159396eecf5c8336ae4
crown0205 <crown0205@gmail.com>
2024-06-30 22:51:41 +0900

    design : tag 추가

--- src/app/(blog)/page.tsx
+++ src/app/(blog)/page.tsx
@@ -1,12 +1,36 @@
 import { posts } from "#site/contents";
 import { cn, sortPost } from "@/lib/utils";
 import PostItem from "./components/PostItem";
+import { Badge } from "@/components/ui/badge";
+
+interface ITag {
+  tag: string;
+  count: number;
+}
 
 // [ ] : route를 이렇게 하는게 최선인지?
 const BlogPage = async () => {
   const postsData = sortPost(posts.filter((post) => post.published));
 
-  console.log({ postsData: postsData.map((post) => post.tags) });
+  const tags = postsData
+    .map((post) => post.tags)
+    .flat()
+    .reduce<ITag[]>(
+      (acc: ITag[], tag) => {
+        const existingTag = acc.find((t) => t.tag === tag);
+
+        acc.find((t) => t.tag === "all")!.count += 1;
+
+        if (existingTag) {
+          existingTag.count += 1;
+        } else {
+          acc.push({ tag, count: 1 });
+        }
+
+        return acc;
+      },
+      [{ tag: "all", count: 0 }]
+    );
 
   return (
     <div
@@ -16,10 +40,10 @@ const BlogPage = async () => {
         "lg:py-10"
       )}
     >
-      <div className="flex items-center">
+      <div className="flex flex-col">
         <div
           className={cn(
-            "flex flex-col flex-1 gap-3", //
+            "flex flex-col flex-1 gap-3 mb-5", //
             "md:gap-6"
           )}
         >
@@ -37,6 +61,16 @@ const BlogPage = async () => {
             My ramblings on all things web dev.
           </p>
         </div>
+        <section className="flex gap-2 flex-wrap">
+          {tags.map((tag) => (
+            <Badge
+              key={tag.tag}
+              className="rounded-sm cursor-pointer px-3 py-1 text-sm"
+            >
+              {tag.tag} ({tag.count})
+            </Badge>
+          ))}
+        </section>
       </div>
       <hr className="mt-8" />
       <ul>
