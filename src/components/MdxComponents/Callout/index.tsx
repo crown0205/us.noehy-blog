0c77e98dd2ccb6b1d648090ed12d22e96502f62d
crown0205 <crown0205@gmail.com>
2024-07-03 23:56:31 +0900

    docs : interview - js

--- src/components/MdxComponents/Callout/index.tsx
+++ src/components/MdxComponents/Callout/index.tsx
@@ -19,13 +19,15 @@ const Callout = (props: ICalloutProps) => {
             "dark:border-[#009deb77] dark:bg-[#079AE31a] dark:text-[#D7F3FA]",
             "border-l-4 border-[#079ae372]",
             "flex items-center gap-5",
-            "mb-5 px-4 py-6"
+            "mb-5 px-4 py-6 break-words break-keep "
           )}
           role="alert"
         >
           <Icons.info className="min-w-4 min-h-4 md:min-w-6 md:min-h-6" />
           <div className="flex flex-col gap-1">
-            {title ? <p className="font-bold m-0">{title}</p> : null}
+            {title ? (
+              <p className="font-bold m-0 text-xl mb-[8px]">{title}</p>
+            ) : null}
             <div className="m-0 [&_p]:m-0">{children}</div>
           </div>
         </div>
@@ -38,13 +40,15 @@ const Callout = (props: ICalloutProps) => {
             "dark:border-[#f0bb6c84] dark:bg-[#F0BB6C24] dark:text-[#EDE7DA]",
             "border-l-4 border-[#f7be687d]",
             "flex items-center gap-5",
-            "mb-5 px-4 py-6"
+            "mb-5 px-4 py-6 break-words break-keep "
           )}
           role="alert"
         >
           <Icons.warn className="min-w-4 min-h-4 md:min-w-6 md:min-h-6" />
           <div className="flex flex-col gap-1">
-            {title ? <p className="font-bold m-0">{title}</p> : null}
+            {title ? (
+              <p className="font-bold m-0 text-xl mb-[8px]">{title}</p>
+            ) : null}
             <div className={cn("m-0 [&_p]:m-0", "[&_p]:text-[#4D361A]")}>
               {children}
             </div>
@@ -59,13 +63,15 @@ const Callout = (props: ICalloutProps) => {
             "dark:border-[#f7463984] dark:bg-[#F746391a] dark:text-[#F7E5E4]",
             "border-l-4 border-[#F746391a]",
             "flex items-center gap-5",
-            "mb-5 px-4 py-6"
+            "mb-5 px-4 py-6 break-words break-keep "
           )}
           role="alert"
         >
           <Icons.danger className="min-w-4 min-h-4 md:min-w-6 md:min-h-6" />
           <div className="flex flex-col gap-1">
-            {title ? <p className="font-bold m-0">{title}</p> : null}
+            {title ? (
+              <p className="font-bold m-0 text-xl mb-[8px]">{title}</p>
+            ) : null}
             <div className={cn("m-0 [&_p]:m-0", "[&_p]:text-[#821006]")}>
               {children}
             </div>
@@ -80,14 +86,16 @@ const Callout = (props: ICalloutProps) => {
             "dark:border-[#84868892] dark:bg-[#2b2e33] dark:text-[#eaebee]",
             "border-l-4 border-[#12141671]",
             "flex items-center gap-5",
-            "mb-5 px-4 py-6",
+            "mb-5 px-4 py-6 break-words break-keep ",
             className
           )}
           role="alert"
         >
           <Icons.text className="min-w-4 min-h-4 md:min-w-6 md:min-h-6" />
           <div className="flex flex-col gap-1">
-            {title ? <p className="font-bold m-0">{title}</p> : null}
+            {title ? (
+              <p className="font-bold m-0 text-xl mb-[8px]">{title}</p>
+            ) : null}
             <div className={cn("m-0 [&_p]:m-0", "[&_p]:text-[#212124]")}>
               {children}
             </div>
