84268ec5fcc0d46d0d9757b37325df64c8f39a98
crown0205 <crown0205@gmail.com>
2024-07-08 22:44:52 +0900

    feat : pageNation 기능 추가

--- src/app/(blog)/page.tsx
+++ src/app/(blog)/page.tsx
@@ -1,9 +1,11 @@
-import PostTemplate from "./components/PostTemplate";
+import PostTemplate, { IPostTemplateProps } from "./components/PostTemplate";
+
+export interface IBlogPageProps extends IPostTemplateProps {}
 
 // [x] : route를 이렇게 하는게 최선인지?
 // [ ] : category, tag 로 쿼리를 받아 넘기도록 변경해도 좋을듯 하지만 postTemplate은 변경을 못할듯...
-const BlogPage = async () => {
-  return <PostTemplate />;
+const BlogPage = async ({ searchParams }: IBlogPageProps) => {
+  return <PostTemplate searchParams={searchParams} />;
 };
 
 export default BlogPage;
--- src/components/common/PageNation/index.tsx
+++ src/components/common/PageNation/index.tsx
@@ -1,28 +1,50 @@
 "use client";
 
+import { IPostListProps } from "@/app/(blog)/components/PostList";
+import usePosts from "@/hooks/usePosts";
 import { cn } from "@/lib/utils";
-import { useState } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
 
-const PageNation = () => {
-  const pages = [1, 2, 3];
-  const [activePave, setPage] = useState(1);
-  // [ ] : 페이지네이션 로직 추가
+export const POSTS_PER_PAGE = 8;
+
+interface IPageNationProps extends IPostListProps {}
+
+// [ ] : PageNation 컴포넌트 수정 query 받아서 변경하도록 수정
+const PageNation = ({ currentPage }: IPageNationProps) => {
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+
+  const { postsData } = usePosts({ pathName });
+  const totalPages = Math.ceil(postsData.length / POSTS_PER_PAGE);
+
+  const createPageUrl = (page: number) => {
+    if (currentPage === page) return;
+
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    return `${pathName}?${params.toString()}`;
+  };
 
   return (
     <div className="w-full flex justify-center mt-8">
-      {pages.map((page) => (
-        <button
-          key={page}
-          className={cn(
-            "px-2 py-1 mx-1 border border-gray-300 rounded-md",
-            "hover:bg-gray-100 hover:border-gray-400 hover:text-gray-800",
-            "active:bg-gray-200 active:border-gray-500 active:text-gray-900",
-            page === activePave ? "bg-gray-200" : ""
-          )}
-        >
-          {page}
-        </button>
-      ))}
+      {Array(totalPages)
+        .fill("")
+        .map((_, idx) => (
+          <button
+            key={idx}
+            className={cn(
+              "px-2 py-1 mx-1 border border-gray-300 rounded-md",
+              idx + 1 !== currentPage &&
+                "hover:bg-gray-100 hover:border-gray-400 hover:text-gray-800",
+              idx + 1 !== currentPage &&
+                "active:bg-gray-200 active:border-gray-500 active:text-gray-900",
+              idx + 1 === currentPage ? "bg-gray-200" : ""
+            )}
+            disabled={idx + 1 === currentPage}
+          >
+            <a href={createPageUrl(idx + 1)}>{idx + 1}</a>
+          </button>
+        ))}
     </div>
   );
 };
